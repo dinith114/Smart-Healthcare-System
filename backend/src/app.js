b6e920941cc4defe19ed603356153d73179dd598
Jayangi18 <jayangizasso@gmail.com>
2025-10-16 12:52:10 +0530

    Initially added appointmentModel, appointmentController, appointmentRoute, slotUtils, notificationServie & changes to app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,10 +3,16 @@ const cors = require("cors");
 const morgan = require("morgan");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 if (process.env.NODE_ENV !== "test") app.use(morgan("dev"));
 
 app.get("/api/health", (_req, res) => res.json({ ok: true }));
+
+const appointmentRoutes = require("./routes/appointmentRoutes");
+
+app.use("/api/appointments", appointmentRoutes);
+
 
 module.exports = app;
